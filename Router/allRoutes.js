64b7e4099d0f7ac2b00e646319d5855714ed90cd
Jorge Hereaud <j.reaud.dev@gmail.com>
2024-09-29 11:02:43 +0200

    Add marais.html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/habitats", "Habitas", "/pages/habitats.html"),
     new Route("/page", "Page", "/pages/page.html"),
+    new Route("/marais", "Marais", "/pages/marais.html"),
     
 ];
 
