495a7aa1542b3937caf5b9c7d9a2c40386ae2529
Jorge Hereaud <j.reaud.dev@gmail.com>
2024-10-10 13:21:45 +0200

    token auth. ajouté en dur. script.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,7 @@ export const allRoutes = [
     new Route("/marais", "Marais", "/pages/marais.html"),
     new Route("/savane", "Savane", "/pages/savane.html"),
     new Route("/jungle", "Jungle", "/pages/jungle.html"),
-    new Route("/signin", "Sign-in", "/pages/auth/signin.html"),
+    new Route("/signin", "Sign-in", "/pages/auth/signin.html", "/js/auth/signin.js"),
     new Route("/signup", "Sign-up", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editpassword", "Changement mot de passe", "/pages/auth/editpassword.html"),
---
+++ js/auth/signin.js
@@ -0,0 +1,26 @@
+const mailInput = document.getElementById("emailInput");
+const passwordInput = document.getElementById("passwordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    // appeler l'api pour vérifier les credentials en bdd. 
+    
+    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+        alert("Vous êtes connecté.");
+
+        // Il faudra récupérer le vrai token
+        const token = "jqlkdjfqlskfjqlfkjqmlkqjfmldkjqmlkdfj";
+        setToken(token);
+
+        //placer le token en coockie:
+
+
+        window.location.replace("/"); // ceci permet de rediriger l'utilisateur vers la page d'accueil.
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
