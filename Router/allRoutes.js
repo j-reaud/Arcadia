60b0bfc65accc9e064e5878edd471bfe1135cb7b
Jorge Hereaud <j.reaud.dev@gmail.com>
2024-11-16 07:04:55 +0100

    refont bug pag signup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,11 +8,13 @@ export const allRoutes = [
     new Route("/savane", "Savane", "/pages/savane.html", []),
     new Route("/jungle", "Jungle", "/pages/jungle.html", []),
     new Route("/signin", "Signin", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/signup", "Signup", "/pages/auth/signup.html" ["disconnected"], "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html" ["client", "admin"]),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+   
     new Route("/editpassword", "Changement mot de passe", "/pages/auth/editpassword.html" ["client", "admin"]),
     
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
+
+//["client", "admin"]
